5ad5a8350eb23cf051b872b61b7485fffd9c933a
Mohammad Ridwanul Alam <myworld.shoyab@gmail.com>
2022-07-27 00:01:04 +0600

    jul 27

---
+++ intro-cpp/print_flat.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double a = 2.34634243;
+    char az[5] = "hi";
+    cout << fixed << setprecision(4) << a << endl;
+    cout << az << endl;
+    return 0;
+}
